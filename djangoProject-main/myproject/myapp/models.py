281fbf91a3b881e92ddd85713e50656ed61203da
jimmy <jimmy17-18@hotmail.com>
2023-04-28 16:40:13 +0100

    added documentation to functions/classes

--- djangoProject-main/myproject/myapp/models.py
+++ djangoProject-main/myproject/myapp/models.py
@@ -2,23 +2,57 @@ from django.db import models
 
 # Create your models here.
 class Album(models.Model):
+    """
+    A model class representing an album.
+
+    Attributes:
+    title (str): The title of the album.
+    release_date (date): The date the album was released.
+    description (str): A brief description of the album.
+
+    Methods:
+    __str__(): Returns the title of the album as a string.
+
+    """
     title = models.CharField(max_length=100)
     release_date = models.DateField()
     description = models.TextField()
 
     def __str__(self):
+        """
+        Return a string representation of the album object.
+        """
         return self.title
     
 class Tour(models.Model):
+    """
+    A class representing a music tour.
+
+    Attributes:
+        title (str): The title of the tour.
+        location (str): The location of the tour.
+        date (date): The date of the tour.
+        description (str): A description of the tour.
+    """
     title = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     date = models.DateField()
     description = models.TextField()
 
     def __str__(self):
+        """
+        Return a string representation of the tour object.
+        """
         return self.title
     
 class BandHistory(models.Model):
+    """
+    A class representing the band's history.
+
+    Attributes:
+        year (int): The year the event occurred.
+        event (str): A description of the event.
+    """
     year= models.IntegerField()
     event = models.TextField()
     
@@ -26,13 +60,27 @@ class BandHistory(models.Model):
         verbose_name_plural = "Band History"
 
     def __str__(self):
+        """
+        Return a string representation of the band history object.
+        """
         return f"{self.year}: {self.event}"
 
 class BandMember(models.Model):
+    """
+    A class representing a band member.
+
+    Attributes:
+        name (str): The name of the band member.
+        instrument (str): The instrument the band member plays.
+        year_joined (int): The year the band member joined (optional).
+    """
     name = models.CharField(max_length=100)
     instrument = models.CharField(max_length=50)
     year_joined = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
+        """
+        Return a string representation of the band member object.
+        """
         return self.name
 
--- djangoProject-main/myproject/myapp/views.py
+++ djangoProject-main/myproject/myapp/views.py
@@ -5,25 +5,43 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 
 def homepage(request):
+    """ 
+    Renders the 'homepage.html' template. No input parameters are required.
+    """
     return render(request, 'homepage.html')
 
 def album_list(request):
+    """
+    Queries the database for all albums, orders them by release date, and renders the 'album_list.html' template with the list of albums passed as context data.
+    """
     albums = Album.objects.all().order_by('release_date')
     return render(request, 'album_list.html', {'albums': albums})
 
 def tour_list(request):
+    """
+    Queries the database for all tours and renders the 'tour_list.html' template with the list of tours passed as context data.
+    """
     tours = Tour.objects.all()
     return render(request, 'tour_list.html', {'tours': tours})
 
 def band_history(request):
+    """
+    Queries the database for all band history events and renders the 'band_history.html' template with the list of events passed as context data.
+    """
     history = BandHistory.objects.all()
     return render(request, 'band_history.html', {'history': history})
 
 def band_members(request):
+    """
+    Queries the database for all band members, orders them by year joined (if available), and renders the 'band_members.html' template with the list of members passed as context data.
+    """
     members = BandMember.objects.all().order_by('year_joined')
     return render(request, 'band_members.html', {'members': members})
 
 def register(request):
+    """
+    Renders the 'register.html' template with a UserCreationForm instance passed as context data. If the request method is POST, the view validates the form and creates a new user, logs the user in, and redirects to the homepage. If the form is invalid, it re-renders the form with the errors displayed.
+    """
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -35,6 +53,9 @@ def register(request):
     return render(request, 'register.html', {'form': form})
 
 def login_view(request):
+    """
+    Renders the 'login.html' template. If the request method is POST, it attempts to authenticate the user with the provided credentials. If successful, logs the user in, displays a welcome message, and redirects to the homepage. If unsuccessful, displays an error message.
+    """
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -51,5 +72,8 @@ def login_view(request):
     return render(request, 'login.html', {})
 
 def logout_view(request):
+    """
+    Logs the user out and redirects to the homepage.
+    """
     logout(request)
     return redirect('home')
